5fb4f623fa6e75e2eeb6c9317cb8864d7d67d8af
jacobkri <jacobkweb@gmail.com>
2018-06-04 01:22:56 +0200

    Changed all getElementById to querySelector instead (Consistency)

--- tornado.js
+++ tornado.js
@@ -87,7 +87,7 @@ async function showContent() {
   
   //The below code calls "scrollFunction()" to change the color of the burgerMenu when needed
   //Because of design-requirements, this is only needed on some pages. I.e. Some pages has a white #site_header and others has a video or an image.
-  if (!document.getElementById('site_header_follow_black')) {
+  if (!document.querySelector('#site_header_follow_black')) {
 	// If the header background is dark, switch to white burgerBar's in the #burgerMenu
 	window.onscroll = function() {scrollFunction()};
   }
@@ -121,53 +121,53 @@ function create_burger_menu(navigation_array) {
 }
 
 function toggle_burger_menu() {
-	let burger_menu = document.getElementById("burgerMenu");
-	let menu_list   = document.getElementById("menuList");
+	let burger_menu = document.querySelector("#burgerMenu");
+	let menu_list   = document.querySelector("#menuList");
 	
 	if (menu_state_open == true) {
 	  burger_menu.className = "menuClosed";
 	  toggleBurgerColor(); // When menu is closed, find out if we need to change burgerBar colors
-	  document.getElementById("burgerMenuButton").disabled = true; // disable button while animation runs
+	  document.querySelector("#burgerMenuButton").disabled = true; // disable button while animation runs
 	  
 	  setTimeout(function(){ // After the animation 
 	    burger_menu.className = "";
 	    // document.querySelector("body").style.overflowY = "auto"; // Shows the scrollbar when menu is closed
-	    document.getElementById("burgerMenuButton").disabled = false; // enable button when animation is done
+	    document.querySelector("burgerMenuButton").disabled = false; // enable button when animation is done
 	  }, 1000);
 	  
 	  menu_state_open = false;
 	  
-	  document.getElementById('site_header_logo_black_menu').style.display = "none"; // Instant-Hide black logo in menu
-		document.getElementById('site_header_follow_black_menu').style.display = "none";  // Instant-Hide black social icons in menu
+	  document.querySelector('#site_header_logo_black_menu').style.display = "none"; // Instant-Hide black logo in menu
+	  document.querySelector('#site_header_follow_black_menu').style.display = "none";  // Instant-Hide black social icons in menu
 		
 	  // Depending on which page was requested, we need either a white or black logo in #site_header
-	  if(document.getElementById('site_header_logo_white')) {
-	    document.getElementById('site_header_logo_white').style.display = "block";
-	    document.getElementById('site_header_follow_white').style.display = "block";
+	  if(document.querySelector('#site_header_logo_white')) {
+		document.querySelector('#site_header_logo_white').style.display = "block";
+		document.querySelector('#site_header_follow_white').style.display = "block";
 	  } else {
-		document.getElementById('site_header_logo_black').style.display = "block";
-		document.getElementById('site_header_follow_black').style.display = "block";  
+		document.querySelector('#site_header_logo_black').style.display = "block";
+		document.querySelector('#site_header_follow_black').style.display = "block";  
 	  }
 	  
 	} else {
 	  burger_menu.className = "menuOpen";
 	  toggleBurgerColor(); // When menu is opened, change burgerBar colors to black
 	  
-	  document.getElementById("burgerMenuButton").disabled = true; // disable button while animation runs
+	  document.querySelector("#burgerMenuButton").disabled = true; // disable button while animation runs
 	  // Depending on which page was requested, we need either a white or black logo in #site_header
-	  if(document.getElementById('site_header_logo_white')) {
-	    document.getElementById('site_header_logo_white').style.display = "none";
-	    document.getElementById('site_header_follow_white').style.display = "none";
+	  if(document.querySelector('#site_header_logo_white')) {
+		document.querySelector('#site_header_logo_white').style.display = "none";
+		document.querySelector('#site_header_follow_white').style.display = "none";
 	  } else {
-		document.getElementById('site_header_logo_black').style.display = "none";
-		document.getElementById('site_header_follow_black').style.display = "none";  
+		document.querySelector('#site_header_logo_black').style.display = "none";
+		document.querySelector('#site_header_follow_black').style.display = "none";  
 	  }
 	  
 	  setTimeout(function(){
 	    // document.querySelector("body").style.overflowY = "hidden"; // Hides the scrollbar while meny open
-	    document.getElementById("burgerMenuButton").disabled = false; // enable button when animation is done
-		document.getElementById('site_header_logo_black_menu').style.display = "block"; // Show black logo in menu
-		document.getElementById('site_header_follow_black_menu').style.display = "block"; // show black social icons in menu
+		document.querySelector("#burgerMenuButton").disabled = false; // enable button when animation is done
+		document.querySelector('#site_header_logo_black_menu').style.display = "block"; // Show black logo in menu
+		document.querySelector('#site_header_follow_black_menu').style.display = "block"; // show black social icons in menu
 	  }, 1000);
 	 //  menu_list.className = "listOpen";
 	  menu_state_open = true;
@@ -175,7 +175,7 @@ function toggle_burger_menu() {
 	
 }
 function add_event_listeners() {
-  let burger_menu = document.getElementById("burgerMenuButton");
+  let burger_menu = document.querySelector("#burgerMenuButton");
   burger_menu.addEventListener('click', toggle_burger_menu, false);
 }
 
@@ -192,15 +192,15 @@ function scrollFunction() {
 function toggleBurgerColor() {
 	
 	let chosenColor = '#000';
-	if ((document.getElementById('site_header_logo_white')) && (menu_state_open!==false)) {
+	if ((document.querySelector('#site_header_logo_white')) && (menu_state_open!==false)) {
 	  // Change burgerBar color to white if burgerMenu is open AND element ID "site_header_logo_white" exists
 	  chosenColor = '#fff';
 	} else {
 	  chosenColor = '#000'; // Else we change color of burgerBars to black
 	}
-	document.getElementById('burgerBar1').style.background = chosenColor;
-	document.getElementById('burgerBar2').style.background = chosenColor;
-	document.getElementById('burgerBar3').style.background = chosenColor;
+	document.querySelector('#burgerBar1').style.background = chosenColor;
+	document.querySelector('#burgerBar2').style.background = chosenColor;
+	document.querySelector('#burgerBar3').style.background = chosenColor;
 }
 
 
@@ -235,8 +235,8 @@ function GetURLParameter(sParam) {
 
 function initMap() {
     // Styles a map in night mode.
-	var locationLadies = {lat: 55.6919103, lng: 12.5453035};  
-    var map = new google.maps.Map(document.getElementById('map'), {
+	let locationLadies = {lat: 55.6919103, lng: 12.5453035};  
+    let map = new google.maps.Map(document.querySelector('#map'), {
       center: locationLadies,
       zoom: 17,
       styles: [
@@ -400,7 +400,7 @@ function initMap() {
     		  }
     		]
     });
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
         position: locationLadies,
         map: map,
         title: 'Ladyfingers'
