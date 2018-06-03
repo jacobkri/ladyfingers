7c6d197f2a4d039d96dfdc264675c367905e5040
jacobkri <jacobkweb@gmail.com>
2018-06-03 22:59:07 +0200

    Added Google Map via API - hopefully final commit.

--- tornado.js
+++ tornado.js
@@ -83,6 +83,7 @@ async function showContent() {
 
   load_instagram_plugin(); // Runs the Instagram Gallery Wordpress Plugin
   add_event_listeners(); // Add <button> Event Listeners after loading content
+  initMap();
   
   //The below code calls "scrollFunction()" to change the color of the burgerMenu when needed
   //Because of design-requirements, this is only needed on some pages. I.e. Some pages has a white #site_header and others has a video or an image.
@@ -232,7 +233,179 @@ function GetURLParameter(sParam) {
 
 
 
-
+function initMap() {
+    // Styles a map in night mode.
+	var locationLadies = {lat: 55.6919103, lng: 12.5453035};  
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: locationLadies,
+      zoom: 17,
+      styles: [
+    	  {
+    		    "elementType": "geometry",
+    		    "stylers": [
+    		      {
+    		        "color": "#f5f5f5"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "elementType": "labels.icon",
+    		    "stylers": [
+    		      {
+    		        "visibility": "off"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "elementType": "labels.text.fill",
+    		    "stylers": [
+    		      {
+    		        "color": "#616161"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "elementType": "labels.text.stroke",
+    		    "stylers": [
+    		      {
+    		        "color": "#f5f5f5"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "administrative.land_parcel",
+    		    "elementType": "labels.text.fill",
+    		    "stylers": [
+    		      {
+    		        "color": "#bdbdbd"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "poi",
+    		    "elementType": "geometry",
+    		    "stylers": [
+    		      {
+    		        "color": "#eeeeee"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "poi",
+    		    "elementType": "labels.text.fill",
+    		    "stylers": [
+    		      {
+    		        "color": "#757575"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "poi.park",
+    		    "elementType": "geometry",
+    		    "stylers": [
+    		      {
+    		        "color": "#e5e5e5"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "poi.park",
+    		    "elementType": "labels.text.fill",
+    		    "stylers": [
+    		      {
+    		        "color": "#9e9e9e"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "road",
+    		    "elementType": "geometry",
+    		    "stylers": [
+    		      {
+    		        "color": "#ffffff"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "road.arterial",
+    		    "elementType": "labels.text.fill",
+    		    "stylers": [
+    		      {
+    		        "color": "#757575"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "road.highway",
+    		    "elementType": "geometry",
+    		    "stylers": [
+    		      {
+    		        "color": "#dadada"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "road.highway",
+    		    "elementType": "labels.text.fill",
+    		    "stylers": [
+    		      {
+    		        "color": "#616161"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "road.local",
+    		    "elementType": "labels.text.fill",
+    		    "stylers": [
+    		      {
+    		        "color": "#9e9e9e"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "transit.line",
+    		    "elementType": "geometry",
+    		    "stylers": [
+    		      {
+    		        "color": "#e5e5e5"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "transit.station",
+    		    "elementType": "geometry",
+    		    "stylers": [
+    		      {
+    		        "color": "#eeeeee"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "water",
+    		    "elementType": "geometry",
+    		    "stylers": [
+    		      {
+    		        "color": "#c9c9c9"
+    		      }
+    		    ]
+    		  },
+    		  {
+    		    "featureType": "water",
+    		    "elementType": "labels.text.fill",
+    		    "stylers": [
+    		      {
+    		        "color": "#9e9e9e"
+    		      }
+    		    ]
+    		  }
+    		]
+    });
+    var marker = new google.maps.Marker({
+        position: locationLadies,
+        map: map,
+        title: 'Ladyfingers'
+    });
+  }
 
 
 
